84b7811851ece2e53de335f4c062eb4402f430cb
dkchmielewski <66046504+dkchmielewski@users.noreply.github.com>
2023-01-02 14:08:11 -0500

    anonymous functions

---
+++ app.js
@@ -0,0 +1,9 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+// const start = function(){
+//     console.log('Game is starting...');
+// };
+
+startGameBtn.addEventListener('click', function() {
+    console.log('Game is starting...');
+});
