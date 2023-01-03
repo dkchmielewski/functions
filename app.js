7e4b666a1b3e4dfefd28b517a310fcad55c184e3
dkchmielewski <66046504+dkchmielewski@users.noreply.github.com>
2023-01-02 20:34:53 -0500

    adding user choices to the game

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-const startGameBtn = document.getElementById('start-game-btn');
+const startGameBtn = document.getElementById("start-game-btn");
 
-// const start = function(){
-//     console.log('Game is starting...');
-// };
+const ROCK = "ROCK";
+const PAPER = "PAPER";
+const SCISSORS = "SCISSORS";
+const DEFAULT_USER_CHOICE = ROCK;
 
-startGameBtn.addEventListener('click', function() {
-    console.log('Game is starting...');
+let gameIsRunning = false;
+
+const getPlayerChoice = function () {
+  const selection = prompt(
+    `${ROCK}, ${PAPER} or ${SCISSORS}?`,
+    ""
+  ).toUpperCase();
+  if (selection !== ROCK && selection !== SCISSORS && selection !== PAPER) {
+    alert(`Invalid choice! We chose ${DEFAULT_USER_CHOICE} for you!`);
+    return DEFAULT_USER_CHOICE;
+  }
+  return selection;
+};
+
+startGameBtn.addEventListener("click", function () {
+  if (gameIsRunning) {
+    return;
+  }
+  gameIsRunning = true;
+  console.log("Game is starting...");
+  const playerSelection = getPlayerChoice();
+  console.log(playerSelection);
 });
